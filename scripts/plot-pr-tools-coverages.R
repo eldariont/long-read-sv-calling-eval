158be2a69a27f673c780c13ba0f3388592e4f747
David Heller <heller_d@molgen.mpg.de>
2019-02-21 09:07:16 +0100

    improve pr plots

--- scripts/plot-pr-tools-coverages.R
+++ scripts/plot-pr-tools-coverages.R
@@ -7,9 +7,10 @@ res <- read_tsv(args[1], col_names = c("tool", "sample", "min", "metric", "value
 res %>%
     mutate(fraction = str_match(sample, ".*\\.subsampled\\.(.*)")[2]) %>%
     spread(key = metric, value = value) %>%
+    filter(precision != 0 | recall != 0) %>%
     ggplot(aes(precision, recall, color=sample)) +
       geom_point(size=1.5) +
-      geom_line() +
+      geom_path() +
       theme_bw()
 
-ggsave(args[2], width=10, height=5)
+ggsave(args[2], width=6, height=5)
--- scripts/plot-pr-tools.R
+++ scripts/plot-pr-tools.R
@@ -6,9 +6,11 @@ res <- read_tsv(args[1], col_names = c("tool", "sample", "min", "metric", "value
 
 res %>%
     spread(key = metric, value = value) %>%
+    filter(precision!=0 | recall!=0) %>%
     ggplot(aes(precision, recall, color=tool)) +
       geom_point(size=1.5) +
-      geom_line() +
+      geom_path() +
+      coord_cartesian(xlim=c(0,1), ylim=c(0,1)) +
       theme_bw()
 
-ggsave(args[2], width=10, height=5)
+ggsave(args[2], width=6, height=5)
