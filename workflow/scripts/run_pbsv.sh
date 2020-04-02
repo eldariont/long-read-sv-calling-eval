4073cc78694b8bde108bc5d446bcad0e4457ff82
David Heller <heller_d@molgen.mpg.de>
2020-04-02 22:13:46 +0200

    run pbsv discover only once

--- workflow/scripts/run_pbsv.sh
+++ workflow/scripts/run_pbsv.sh
@@ -19,9 +19,9 @@ BAM_NAME="$(basename -- $BAM)"
 GENOME_NAME="$(basename -- $GENOME)"
 mkdir -p $MXQ_JOB_TMPDIR/output
 echo "Start pbsv discover"
-seq $FROM $STEP $TO | xargs -I MINSUPP -P $THREADS -n 1 pbsv discover $MXQ_JOB_TMPDIR/input/$BAM_NAME $MXQ_JOB_TMPDIR/output/min_MINSUPP.svsig.gz
+pbsv discover $MXQ_JOB_TMPDIR/input/$BAM_NAME $MXQ_JOB_TMPDIR/output/signatures.svsig.gz
 echo "Start pbsv call"
-seq $FROM $STEP $TO | xargs -I MINSUPP -P $THREADS -n 1 pbsv call -t INS,DEL -j 1 --min-sv-length $MINLEN --max-ins-length 100K --call-min-reads-one-sample MINSUPP --call-min-reads-all-samples MINSUPP --call-min-reads-per-strand-all-samples 0 --call-min-bnd-reads-all-samples 0 --call-min-read-perc-one-sample 0 $MXQ_JOB_TMPDIR/input/$GENOME_NAME $MXQ_JOB_TMPDIR/output/min_MINSUPP.svsig.gz $MXQ_JOB_TMPDIR/output/min_MINSUPP.vcf
+seq $FROM $STEP $TO | xargs -I MINSUPP -P $THREADS -n 1 pbsv call -t INS,DEL -j 1 --min-sv-length $MINLEN --max-ins-length 100K --call-min-reads-one-sample MINSUPP --call-min-reads-all-samples MINSUPP --call-min-reads-per-strand-all-samples 0 --call-min-bnd-reads-all-samples 0 --call-min-read-perc-one-sample 0 $MXQ_JOB_TMPDIR/input/$GENOME_NAME $MXQ_JOB_TMPDIR/output/signatures.svsig.gz $MXQ_JOB_TMPDIR/output/min_MINSUPP.vcf
 
 OUTDIR=$9
 echo "Copy back to $OUTDIR"
